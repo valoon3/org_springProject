5f563d1a1d1e7fd64861502c5d3a07d8036c1578
NaByungHo <86526945+valoon3@users.noreply.github.com>
2022-03-26 13:56:47 +0900

    Add files via upload

---
+++ spring_noname/src/main/java/myapp/controller/mainController.java
@@ -0,0 +1,21 @@
+package myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class mainController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+
+        return "index";
+    }
+
+    @RequestMapping(value = "/test")
+    public String test() {
+        return "test";
+    }
+
+
+}
---
+++ spring_noname/target/spring_noname/WEB-INF/views/test.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: nabyeongho
+  Date: 2022/03/26
+  Time: 1:42 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+  오늘은 친구들을 만나는 날 ㅁㄴㅇㄹㅁㄴㅇ
+</body>
+</html>
